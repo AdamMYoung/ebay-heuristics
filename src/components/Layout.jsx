6acde5691ce8901fa1ebfa5b192b15994d6cc24f
Adam Young <adamyoungg97@gmail.com>
2020-04-30 23:14:38 +0100

    Styling changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import { NavBar } from './navigation/NavBar';
 import { Routes } from './Routes';
 
 export const Layout = () => {
     return (
-        <div>
-            <NavBar />
-            <div style={{ margin: 12 }}>
-                <Routes />
-            </div>
+        <div style={{ background: '#191a1a' }}>
+            <Routes />
         </div>
     );
 };
---
+++ src/components/controls/MapControls.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export const MapControls = () => {
+    return (
+        <div>
+            <Button variant="outline-light">Login to eBay</Button>
+        </div>
+    );
+};
